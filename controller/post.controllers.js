743bf61ef56043f9c61ca372b4eb295a1adb505c
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-10-14 15:24:21 +0300

    post

--- controller/post.controllers.js
+++ controller/post.controllers.js
@@ -52,10 +52,10 @@ exports.CreateAPost = async (req, res) => {
     const numberOfWords = body.trim().split(/\s+/).length;
     const readTime = Math.ceil(numberOfWords / wpm);
 
-    let {firstname, lastname} = req.user
+    let { firstname, lastname } = req.user
     let author = `${firstname} ${lastname}`
     let authorId = req.user._id
-  
+
     const post = new Post({
       title,
       description,
@@ -79,3 +79,60 @@ exports.CreateAPost = async (req, res) => {
   }
 }
 
+exports.UpdateAPost = async (req, res) => {
+  const { state, body } = req.body
+  try {
+    const post = await Post.findByIdAndUpdate(
+      req.parms.postId,
+      { state, body },
+      { new: true }
+    )
+
+    //check if post belongs to the user initiatin the request 
+    if (post.authorId.toString() !== req.user._id) {
+      return res.status(401).json({
+        status: 'Fail',
+        message: `You can only update a post you created!`
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      post
+    });
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.deleteAPost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndRemove(req.params.postId, {
+      authorId: req.user.id,
+    });
+    if (!post) return res.status(404).json({
+      status: 'Fail',
+      message: 'Post with given Id not found'
+    })
+
+    if (post.authorId.toString() !== req.user.id) {
+      return res.status(401).json({
+        status: "Fail",
+        message: `You can only delete a post you created!`,
+      });
+    }
+
+    //delete post from 'posts' array in user the document
+    const postByUser = await User.findById(req.user._id);
+    postByUser.posts.pull(post._id);
+    await postByUser.updateOne({ posts: postByUser.posts });
+
+    //return deleted post
+    res.status(200).json({
+      status: "success",
+      message: "Post deleted successfully",
+    });
+  } catch (err) {
+    throw err;
+  }
+};
